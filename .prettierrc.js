df5503aca845c5ac0f38ff925b8531afd57d34fb
Sanchan Moses <sanchanm@wekancode.com>
2020-03-03 15:59:33 +0000

    chore(prettier): base prettier configuration

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 2,
+  bracketSpacing: true,
+  bracesSpacing: true,
+  arrowParens: 'always',
+};
