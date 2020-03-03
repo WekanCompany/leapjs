913a369e2d866e944a043a9d874626b53d8d21a1
Sanchan Moses <sanchanm@wekancode.com>
2020-03-03 21:49:03 +0000

    feat(scheduler): job mode + status and log status types

---
+++ packages/scheduler/src/types/index.ts
@@ -0,0 +1,3 @@
+export type schedulerJobMode = 'once' | 'recurring';
+export type schedulerJobStatus = 'scheduled' | 'triggered' | 'completed';
+export type schedulerJobLogStatus = 'success' | 'failure';
