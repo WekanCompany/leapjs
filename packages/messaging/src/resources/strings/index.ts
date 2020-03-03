695d6c8548bc6c84562d615c846c4957e398a560
Sanchan Moses <sanchanm@wekancode.com>
2020-03-03 21:31:31 +0000

    feat(messaging): error strings

---
+++ packages/messaging/src/resources/strings/index.ts
@@ -0,0 +1,11 @@
+const ERROR_RENDERING_TEMPLATE = 'Failed to render template';
+const ENGINE_NOT_SUPPORTED = 'Engine not supported';
+const QUEUE_SEND_FAILED = 'Unable to send message to queue';
+const INVALID_ATTACHMENTS = 'Attachments must be an array of file paths';
+
+export {
+  ERROR_RENDERING_TEMPLATE,
+  ENGINE_NOT_SUPPORTED,
+  QUEUE_SEND_FAILED,
+  INVALID_ATTACHMENTS,
+};
