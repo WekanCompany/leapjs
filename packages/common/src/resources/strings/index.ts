6f0237fa81307ce41bfb38c1683b5c1916f01963
Sanchan Moses <sanchanm@wekancode.com>
2020-03-03 21:08:29 +0000

    feat(common): error strings

---
+++ packages/common/src/resources/strings/index.ts
@@ -0,0 +1,15 @@
+const INVALID_DECORATOR_OPERATION =
+  '@inject must be applied to a constructor parameter or class property';
+
+const DUPLICATED_METADATA = 'Metadata key already exists';
+const DUPLICATED_INJECTABLE_DECORATOR =
+  'Cannot apply @injectable multiple times';
+
+const INVALID_INJECTION = 'Error injecting `undefined`';
+
+export {
+  INVALID_DECORATOR_OPERATION,
+  DUPLICATED_METADATA,
+  DUPLICATED_INJECTABLE_DECORATOR,
+  INVALID_INJECTION,
+};
