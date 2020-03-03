4b12cfbbf4033dd13e390c906063243ee5e6b28b
Sanchan Moses <sanchanm@wekancode.com>
2020-03-03 21:31:01 +0000

    feat(messaging): added sendgrid support

---
+++ packages/messaging/src/sendgrid/index.ts
@@ -0,0 +1,61 @@
+import { Logger } from '@leapjs/common';
+import { setApiKey, send as sendOne, sendMultiple } from '@sendgrid/mail';
+import { MailData } from '@sendgrid/helpers/classes/mail';
+import { readFileSync } from 'fs';
+import { basename, resolve } from 'path';
+import { lookup } from 'mime-types';
+import { IMailTransport } from '../interfaces/mail';
+import { INVALID_ATTACHMENTS } from '../resources/strings';
+
+class Sendgrid implements IMailTransport {
+  public init(apiKey: string): void {
+    setApiKey(apiKey as any);
+  }
+
+  public async send(message: any): Promise<boolean> {
+    const mail: MailData = { from: '' };
+    mail.from = message.from;
+    mail.to = message.to;
+    mail.subject = message.subject;
+    mail.text = message.text || '';
+    mail.html = message.html;
+
+    try {
+      if (message.attachments) {
+        if (Array.isArray(message.attachments)) {
+          const attachments: any = [];
+
+          for (let i = 0; i < message.attachments.length; i += 1) {
+            const filename = basename(message.attachments[i]);
+            attachments[i] = {
+              content: readFileSync(resolve(message.attachments[i])).toString(
+                'base64',
+              ),
+              filename,
+              type: lookup(filename),
+              disposition: 'attachment',
+            };
+          }
+          mail.attachments = attachments;
+        } else {
+          return Promise.reject(new Error(INVALID_ATTACHMENTS));
+        }
+      }
+    } catch (error) {
+      Logger.error('Error attaching file(s)', error, 'Sendgrid');
+      return false;
+    }
+
+    return sendOne(mail)
+      .then(() => {
+        Logger.log(`Sent mail to ${mail.to} successfully`, 'Sendgrid');
+        return true;
+      })
+      .catch((error) => {
+        Logger.error('Error sending mail', error.response.body, 'Sendgrid');
+        return false;
+      });
+  }
+}
+
+export default Sendgrid;
