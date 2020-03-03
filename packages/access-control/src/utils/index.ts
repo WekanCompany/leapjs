a755b039b01ff57aa525c5c0c06149915d012029
Sanchan Moses <sanchanm@wekancode.com>
2020-03-03 20:50:19 +0000

    refactor(access-control): moved formatPermissionError to utils

---
+++ packages/access-control/src/utils/index.ts
@@ -0,0 +1,5 @@
+function formatPermissionError(method: string, name: string): string {
+  return `You do not have permission to ${method} ${name.split('?')[0]}`;
+}
+
+export { formatPermissionError };
