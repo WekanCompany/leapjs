df2763affae5d00134ac4469c96f8fe9ae7a2580
Sanchan Moses <sanchanm@wekancode.com>
2020-03-03 21:50:56 +0000

    feat(scheduler): service class

---
+++ packages/scheduler/src/services/scheduler.ts
@@ -0,0 +1,242 @@
+import { CronJob } from 'cron';
+import { Logger } from '@leapjs/common';
+import { DocumentType } from '@typegoose/typegoose';
+import { parseExpression } from 'cron-parser';
+import { LeapJobModel, LeapJob } from '../models/job';
+import { LeapJobLogModel } from '../models/job-log';
+import {
+  SchedulerJobStatus,
+  SchedulerLogJobStatus,
+  SchedulerJobMode,
+} from '../constants';
+import { IParserOptions } from '../interfaces';
+import {
+  schedulerJobStatus,
+  schedulerJobMode,
+  schedulerJobLogStatus,
+} from '../types';
+import {
+  INVALID_JOB_DATA,
+  REPEAT_PATTERN_EMPTY,
+  ERROR_JOB_CREATION,
+} from '../resources/strings';
+
+class Scheduler {
+  private frequencyPattern = '* * * * *';
+  private scheduler!: CronJob;
+  private publisher!: any;
+
+  constructor(publisher: any) {
+    this.scheduler = new CronJob(
+      this.frequencyPattern,
+      this.dispatcher,
+      undefined,
+      undefined,
+      undefined,
+      this,
+    );
+    this.publisher = publisher;
+  }
+
+  public static async logJob(
+    job: any,
+    status: schedulerJobLogStatus,
+  ): Promise<void> {
+    new LeapJobLogModel({
+      id: job._id,
+      name: job.name,
+      runOn: job.lastRunOn ? job.lastRunOn : new Date(),
+      type: job.type,
+      data: job.data,
+      status,
+    }).save();
+  }
+
+  private static async updateJobStatus(
+    data: any,
+    status: schedulerJobStatus,
+  ): Promise<void> {
+    const job = 'job' in data ? data.job : data;
+    const { _id } = job;
+
+    if (!_id) {
+      Scheduler.logJob(job, SchedulerLogJobStatus.FAILURE);
+      Promise.reject(new Error(INVALID_JOB_DATA));
+      return;
+    }
+
+    if (!data.reschedule) {
+      LeapJobModel.findByIdAndUpdate(job._id, { status })
+        .then(() => {
+          Scheduler.logJob(job, SchedulerLogJobStatus.SUCCESS);
+        })
+        .catch((error: any) => {
+          Logger.error('Error updating job status', error, 'Scheduler');
+          Scheduler.logJob(job, SchedulerLogJobStatus.FAILURE);
+        });
+    } else {
+      Scheduler.logJob(job, SchedulerLogJobStatus.SUCCESS);
+    }
+  }
+
+  private async dispatcher(): Promise<void> {
+    Logger.log(`Starting up dispatcher ...`, 'Scheduler');
+    if (!this.publisher.isConnected()) {
+      Logger.error('Publisher is undefined', '', 'Scheduler');
+      return;
+    }
+
+    LeapJobModel.find({ status: SchedulerJobStatus.SCHEDULED })
+      .then((result: DocumentType<LeapJob>[]) => {
+        if (result) {
+          result.forEach(async (job: DocumentType<LeapJob>) => {
+            const currentTime = new Date();
+            let reschedule = true;
+
+            if (
+              job.type === SchedulerJobMode.RECURRING &&
+              job.nextRunOn <= currentTime &&
+              job.repeatPattern
+            ) {
+              try {
+                // Handle recurring jobs
+                const options: IParserOptions = {};
+                options.currentDate = job.nextRunOn;
+                if (job.endOn) {
+                  options.endDate = job.endOn;
+                }
+
+                // any needed as a workaround as cron-parser types aren't
+                // defined correctly
+                const pInterval: any = parseExpression(
+                  job.repeatPattern,
+                  options,
+                );
+                const nInterval: any = parseExpression(
+                  job.repeatPattern,
+                  options,
+                );
+                if (!pInterval.hasNext()) {
+                  reschedule = false;
+                }
+
+                // eslint-disable-next-line no-param-reassign
+                job.lastRunOn = currentTime;
+                // eslint-disable-next-line no-param-reassign
+                job.nextRunOn = nInterval.next().toDate();
+                await job.save();
+
+                this.publisher
+                  .queueJob(job.event, job, reschedule)
+                  .catch((error: any) => {
+                    Logger.error('Failed to queue job', error, 'Scheduler');
+                  });
+              } catch (error) {
+                Scheduler.updateJobStatus(job, SchedulerJobStatus.COMPLETED);
+              }
+            } else if (
+              job.type === SchedulerJobMode.ONCE &&
+              job.startOn <= currentTime
+            ) {
+              // Handle one-off jobs
+              // eslint-disable-next-line no-param-reassign
+              job.status = SchedulerJobStatus.TRIGGERED;
+              await job.save();
+
+              this.publisher.queueJob(job.event, job).catch((error: any) => {
+                Logger.error('Failed to queue job', error, 'Scheduler');
+              });
+            }
+          });
+        }
+      })
+      .catch((error) => {
+        Logger.error('Error triggering jobs', error);
+      });
+  }
+
+  public static async rescheduleJob(job: DocumentType<LeapJob>): Promise<void> {
+    Scheduler.updateJobStatus(job, SchedulerJobStatus.SCHEDULED);
+  }
+
+  public static async endJob(job: DocumentType<LeapJob>): Promise<void> {
+    Scheduler.updateJobStatus(
+      job,
+      SchedulerJobStatus.COMPLETED,
+    ).catch((error) => Logger.error(error, '', 'Scheduler'));
+  }
+
+  public start(): void {
+    this.scheduler.start();
+  }
+
+  public async createJob(
+    name: string,
+    event: string,
+    type: schedulerJobMode,
+    data: any,
+    start?: Date,
+    end?: Date,
+    repeatPattern?: string,
+  ): Promise<any> {
+    const startDate = start || new Date();
+    let runOn = start;
+
+    if (end && !repeatPattern) {
+      return Promise.reject(new Error(REPEAT_PATTERN_EMPTY));
+    }
+
+    startDate.setSeconds(0);
+    // eslint-disable-next-line no-unused-expressions
+    end?.setSeconds(0);
+
+    if (repeatPattern) {
+      const options: IParserOptions = {};
+      options.currentDate = startDate;
+      if (end) {
+        options.endDate = end;
+      }
+
+      try {
+        runOn = parseExpression(repeatPattern, options)
+          .next()
+          .toDate();
+      } catch (error) {
+        return Promise.reject(
+          new Error(`${ERROR_JOB_CREATION} - ${error.message}`),
+        );
+      }
+    }
+
+    const job = await new LeapJobModel({
+      name,
+      event,
+      type,
+      data,
+      startOn: startDate,
+      endOn: end || null,
+      repeatPattern: repeatPattern || null,
+      nextRunOn: runOn,
+      status: 'scheduled',
+    }).save();
+    return job._id;
+  }
+
+  public static async cancelJob(jobId: string): Promise<void> {
+    LeapJobModel.findByIdAndUpdate(jobId, {
+      status: SchedulerJobStatus.CANCELLED,
+    })
+      .then(() => {
+        Logger.log(`${jobId} cancelled`, 'Scheduler');
+      })
+      .catch((error: any) => {
+        Logger.error('Error cancelling job', error, 'Scheduler');
+      });
+  }
+
+  public async stop(): Promise<void> {
+    this.scheduler.stop();
+  }
+}
+
+export default Scheduler;
