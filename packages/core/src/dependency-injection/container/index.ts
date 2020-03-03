2a990d38d8c9ed07d3a2e0b148bc6dc9404a73e3
Sanchan Moses <sanchanm@wekancode.com>
2020-03-03 21:23:46 +0000

    feat(core): DI container

---
+++ packages/core/src/dependency-injection/container/index.ts
@@ -0,0 +1,216 @@
+import { parseScript } from 'esprima';
+import {
+  IType,
+  Logger,
+  LEAP_TAGGED_PROPERTIES,
+  LEAP_TAGGED_PARAMETERS,
+  LEAP_PARAM_TYPES,
+  DESIGN_PARAM_TYPES,
+} from '@leapjs/common';
+import { IIdentifier } from '../../interfaces/identifier';
+
+class Container {
+  protected parent: Container | undefined = undefined;
+  // protected options: IContainerOptions;
+
+  protected registry = new Map<Function, any>();
+  private classMetadata: {
+    target: IType<any>;
+    parameters: { name: string; metadata: any[] }[];
+  }[] = [];
+
+  private parameterResolver: any = [];
+  private propertyResolver: any = [];
+
+  private getArguments(func: IType<any>): IIdentifier[] {
+    const functionAsString = func.toString();
+    const tree: any = parseScript(functionAsString);
+    return tree.body[0].body.body[0].value.params;
+  }
+
+  private setParent(parent: Container): void {
+    this.parent = parent;
+  }
+
+  private getMetadata(klass: IType<any>, metadataType: string): any {
+    return Reflect.getMetadata(metadataType, klass) || undefined;
+  }
+
+  private resolveConstructorDependencies(
+    target: IType<any>,
+    Klass: IType<any>,
+    match: any,
+  ): Function {
+    const parameters: any = [];
+    const { length } = match.parameters;
+
+    for (let i = 0; i < length; i += 1) {
+      if (this.registry.has(match.parameters[i].metadata[0].value())) {
+        parameters.push(
+          this.registry.get(match.parameters[i].metadata[0].value()),
+        );
+      } else {
+        parameters.push(match.parameters[i].metadata[0].value());
+        this.parameterResolver.push(parameters);
+        this.parameterResolver.push(Klass);
+        this.parameterResolver.push(target);
+      }
+    }
+
+    return new Klass(...parameters);
+  }
+
+  private resolveAttributeDependency<T>(Klass: IType<any>): T {
+    return new Klass();
+  }
+
+  public createChild(): Container {
+    const container = new Container();
+    container.setParent(this);
+    return container;
+  }
+
+  public resolve<T>(Target: IType<any>): T {
+    const ctorParameters = this.getMetadata(Target, LEAP_TAGGED_PARAMETERS);
+    const classAttributes = this.getMetadata(Target, LEAP_TAGGED_PROPERTIES);
+    const parameters: any = { target: Target, parameters: [] };
+    const parameterTypes = this.getMetadata(Target, LEAP_PARAM_TYPES);
+    const args = this.getArguments(Target);
+
+    const injections: any = [];
+
+    let resolved!: T;
+
+    for (let i = 0; i < args.length; i += 1) {
+      parameters.parameters[i] = {
+        name: args[i].name,
+        metadata: parameterTypes[i],
+      };
+    }
+
+    if (
+      this.classMetadata.find(
+        (metadata: { target: IType<any> }) => metadata.target === Target,
+      ) === undefined
+    ) {
+      this.classMetadata.push(parameters);
+    }
+
+    if (ctorParameters && Object.keys(ctorParameters).length > 0) {
+      let index = 0;
+
+      Object.keys(ctorParameters).forEach((key) => {
+        if (typeof ctorParameters[key][0].value === 'function') {
+          const Dependency = ctorParameters[key][0].value();
+          let dependencyMetadata =
+            Reflect.getMetadata(DESIGN_PARAM_TYPES, Dependency) ||
+            Reflect.getMetadata(LEAP_TAGGED_PROPERTIES, Dependency);
+
+          if (dependencyMetadata && typeof dependencyMetadata === 'object') {
+            [dependencyMetadata] = Object.values(dependencyMetadata);
+          }
+
+          if (dependencyMetadata && dependencyMetadata.length > 0) {
+            for (let j = 0; j < dependencyMetadata.length; j += 1) {
+              const match = this.classMetadata.find(
+                (metadata: { target: IType<any> }) =>
+                  metadata.target === Dependency,
+              );
+              if (match === undefined) {
+                injections[index] = this.resolve<typeof Dependency>(Dependency);
+              } else {
+                injections[index] = this.resolveConstructorDependencies(
+                  Target,
+                  Dependency,
+                  match,
+                );
+              }
+            }
+          } else {
+            injections[index] = new Dependency();
+          }
+          this.registry.set(Dependency, injections[index]);
+          index += 1;
+        } else {
+          injections[index] = ctorParameters[key][0].value;
+        }
+      });
+
+      const { length } = this.parameterResolver;
+
+      for (let x = 0; x < length; x += 3) {
+        const unresolvedDependencies: any = this.registry.get(
+          this.parameterResolver[x + 2],
+        );
+        if (unresolvedDependencies) {
+          let y = 0;
+          Object.keys(
+            unresolvedDependencies[Object.keys(unresolvedDependencies)[0]],
+          ).forEach((key) => {
+            if (this.parameterResolver[x][y]) {
+              unresolvedDependencies[Object.keys(unresolvedDependencies)[0]][
+                key
+              ] = this.registry.get(this.parameterResolver[x][y]);
+              y += 1;
+            }
+          });
+        }
+      }
+
+      const result = new Target(...injections);
+      this.registry.set(Target, result);
+
+      resolved = result;
+    }
+
+    if (classAttributes) {
+      const klass = resolved || new Target();
+      Object.keys(classAttributes).forEach((key) => {
+        if (typeof classAttributes[key][0].value === 'function') {
+          const Dependency = classAttributes[key][0].value();
+          this.registry.set(classAttributes[key][0].value(), new Dependency());
+          const match = this.classMetadata.find(
+            (metadata: { target: IType<any> }) =>
+              metadata.target === Dependency,
+          );
+
+          if (match === undefined) {
+            klass[key] = this.resolve<typeof Dependency>(Dependency);
+            this.registry.set(Dependency, klass[key]);
+          } else {
+            klass[key] = this.registry.get(match.target);
+          }
+          this.propertyResolver.push(key);
+          this.propertyResolver.push(Dependency);
+          this.propertyResolver.push(Target);
+        } else {
+          klass[key] = classAttributes[key][0].value;
+        }
+      });
+
+      const { length } = this.propertyResolver;
+
+      for (let x = 0; x < length; x += 3) {
+        const unresolvedDependencies: any = this.registry.get(
+          this.propertyResolver[x + 2],
+        );
+        if (unresolvedDependencies) {
+          unresolvedDependencies[this.propertyResolver[x]] = this.registry.get(
+            this.propertyResolver[x + 1],
+          );
+        }
+      }
+
+      resolved = klass;
+    }
+
+    if (resolved === undefined) {
+      resolved = new Target();
+    }
+
+    return resolved;
+  }
+}
+
+const container = new Container();
+export default container;
