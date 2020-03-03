7bc6ec063446deb0815e66969dd0cd5f09413a93
Sanchan Moses <sanchanm@wekancode.com>
2020-03-03 21:45:35 +0000

    feat(router): error strings

---
+++ packages/router/src/resources/strings/index.ts
@@ -0,0 +1,5 @@
+const CLASS_NOT_FOUND = 'Controller not found';
+const PARAM_TYPE_METADATA_NOT_FOUND = 'Param type metadata not found';
+const ORIGIN_BLOCKED = 'Origin is blocked by the CORS policy';
+
+export { CLASS_NOT_FOUND, PARAM_TYPE_METADATA_NOT_FOUND, ORIGIN_BLOCKED };
