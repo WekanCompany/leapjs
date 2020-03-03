55b5d344cb300e9e786094bbbd3e3356cb42b424
Sanchan Moses <sanchanm@wekancode.com>
2020-03-03 21:51:11 +0000

    feat(scheduler): error strings

---
+++ packages/scheduler/src/resources/strings/index.ts
@@ -0,0 +1,6 @@
+const INVALID_JOB_DATA = 'Invalid job data provided';
+const REPEAT_PATTERN_EMPTY =
+  'repeatPattern cannot be empty when an end date is provided';
+const ERROR_JOB_CREATION = 'Error creating job';
+
+export { INVALID_JOB_DATA, REPEAT_PATTERN_EMPTY, ERROR_JOB_CREATION };
