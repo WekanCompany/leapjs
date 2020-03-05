078dd4bcc8f4398d25083a12b656a387ba5f7a77
Sanchan Moses <sanchanm@wekancode.com>
2020-03-05 19:21:19 +0000

    feat(access-control): updated package exports

--- packages/access-control/src/index.ts
+++ packages/access-control/src/index.ts
@@ -1,5 +1,7 @@
-import Permission from './classes/permission';
-import acFilterAttributes from './middleware/acl';
+import Permission from './models/permission';
+import acFilterAttributes from './middleware/access-control';
 import accessControl from './decorators/access-control';
+import BaseRole from './models/role';
+import UserRole from './models/user';
 
-export { Permission, accessControl, acFilterAttributes };
+export { Permission, accessControl, acFilterAttributes, BaseRole, UserRole };
