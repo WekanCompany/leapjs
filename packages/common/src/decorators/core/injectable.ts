6abf0bc1ccb97bdb372adf4c0000cb4d5e404680
Sanchan Moses <sanchanm@wekancode.com>
2020-03-05 19:26:27 +0000

    refactor(common): move setCtorParams to utils

--- packages/common/src/decorators/core/injectable.ts
+++ packages/common/src/decorators/core/injectable.ts
@@ -1,20 +1,6 @@
-import { Dictionary, InjectionToken, ctor } from '../../definitions/injector';
-import {
-  DESIGN_PARAM_TYPES,
-  LEAP_PARAM_TYPES,
-  LEAP_TAGGED_PARAMETERS,
-} from '../../constants';
+import { DESIGN_PARAM_TYPES, LEAP_PARAM_TYPES } from '../../constants';
 import { DUPLICATED_INJECTABLE_DECORATOR } from '../../resources/strings';
-
-export function setCtorParams(target: ctor<any>): void {
-  const params: any = Reflect.getMetadata(DESIGN_PARAM_TYPES, target) || [];
-  const injectionTokens: Dictionary<InjectionToken<any>> =
-    Reflect.getOwnMetadata(LEAP_TAGGED_PARAMETERS, target) || {};
-  const { length } = Object.keys(injectionTokens);
-  for (let i = 0; i < length; i += 1) {
-    params[i] = injectionTokens[i];
-  }
-}
+import { setCtorParams } from '../../utils/metadata';
 
 function injectable() {
   return (target: any): any => {
