1e2cace5006f4ac531e0154ee9ee4fafe454ae2b
sanchan moses <sanchanm@wekancode.com>
2020-03-06 14:53:55 +0530

    feat(access-control): updated package exports

--- packages/access-control/src/index.ts
+++ packages/access-control/src/index.ts
@@ -3,5 +3,13 @@ import acFilterAttributes from './middleware/access-control';
 import accessControl from './decorators/access-control';
 import BaseRole from './models/role';
 import UserRole from './models/user';
+import { IDecodedToken } from './interfaces/decoded-token';
 
-export { Permission, accessControl, acFilterAttributes, BaseRole, UserRole };
+export {
+  Permission,
+  accessControl,
+  acFilterAttributes,
+  BaseRole,
+  UserRole,
+  IDecodedToken,
+};
