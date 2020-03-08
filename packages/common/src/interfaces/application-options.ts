9241f14d9da7ecdf9282c44969b04852da6f2e18
Sanchan Moses <sanchanm@wekancode.com>
2020-03-08 16:12:24 +0000

    feat(common): split middleware into before and after middleware

--- packages/common/src/interfaces/application-options.ts
+++ packages/common/src/interfaces/application-options.ts
@@ -1,10 +1,11 @@
 import { CorsOptions } from 'cors';
-import { ILeapContainer } from './container';
+// import { ILeapContainer } from './container';
 import { IConstructor } from './constructor';
 
 export interface ILeapApplicationOptions {
   controllers: IConstructor<any>[];
-  middlewares?: IConstructor<any>[];
+  beforeMiddlewares?: IConstructor<any>[];
+  afterMiddlewares?: IConstructor<any>[];
   prefix?: string;
   corsOptions?: CorsOptions;
   whitelist?: string[];
