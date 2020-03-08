fe6307e2b45fe40a4cb25082d7113b7c056ccc83
Sanchan Moses <sanchanm@wekancode.com>
2020-03-08 16:19:13 +0000

    refactor(router): update error msgs for @controller and @middleware

--- packages/router/src/decorators/controller.ts
+++ packages/router/src/decorators/controller.ts
@@ -1,9 +1,10 @@
 import { LEAP_PARAM_TYPES } from '@leapjs/common';
+import { DUPLICATED_ROUTABLE_DECORATOR } from '../resources/strings';
 
 function Controller(resourceBaseRoute: string): Function {
   return function wrapper(target: Function): void {
     if (Reflect.hasOwnMetadata(LEAP_PARAM_TYPES, target)) {
-      throw new Error('ERRORS_MSGS.DUPLICATED_INJECTABLE_DECORATOR');
+      throw new Error(DUPLICATED_ROUTABLE_DECORATOR);
     }
     Reflect.defineMetadata(
       LEAP_PARAM_TYPES,
--- packages/router/src/resources/strings/index.ts
+++ packages/router/src/resources/strings/index.ts
@@ -2,4 +2,12 @@ const CLASS_NOT_FOUND = 'Controller not found';
 const PARAM_TYPE_METADATA_NOT_FOUND = 'Param type metadata not found';
 const ORIGIN_BLOCKED = 'Origin is blocked by the CORS policy';
 
-export { CLASS_NOT_FOUND, PARAM_TYPE_METADATA_NOT_FOUND, ORIGIN_BLOCKED };
+const DUPLICATED_ROUTABLE_DECORATOR =
+  'Cannot apply multiple decorators (@injectable, @middleware or @controller) to the same object';
+
+export {
+  CLASS_NOT_FOUND,
+  PARAM_TYPE_METADATA_NOT_FOUND,
+  ORIGIN_BLOCKED,
+  DUPLICATED_ROUTABLE_DECORATOR,
+};
