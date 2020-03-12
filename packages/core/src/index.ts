d76cd52fd10f9dc855250a3602b7da3097e59d3f
Sanchan Moses <sanchanm@wekancode.com>
2020-03-12 03:42:05 +0000

    refactor(core): renamed Leap export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import Container from './dependency-injection/container';
-import Leap from './leap';
+import LeapApplication from './leap';
 import MongoDB from './database/mongodb';
 
-export { Container, Leap, MongoDB };
+export { Container, LeapApplication, MongoDB };
