4917870d0c63fcff61db46af87ded35f36726eed
Sanchan Moses <sanchanm@wekancode.com>
2020-03-25 09:31:13 +0000

    feat(crud): added CrudController and @Crud to package exports

--- packages/crud/src/index.ts
+++ packages/crud/src/index.ts
@@ -1,4 +1,6 @@
 import CrudService from './services/crud';
 import buildResultWithPagination from './utils/pagination';
+import CrudController from './controllers/crud';
+import Crud from './decorators/crud-controller';
 
-export { CrudService, buildResultWithPagination };
+export { buildResultWithPagination, CrudService, CrudController, Crud };
