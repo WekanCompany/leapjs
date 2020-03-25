5038473b0d6ca29944a4acdd3695ecc903df40d8
Sanchan Moses <sanchanm@wekancode.com>
2020-03-25 17:10:59 +0000

    refactor(crud): middleware now optional in ICrudControllerMethods

--- packages/crud/src/interfaces/crud-controller-methods.ts
+++ packages/crud/src/interfaces/crud-controller-methods.ts
@@ -1,9 +1,9 @@
 export interface ICrudControllerMethods {
-  createOne: [string, { before?: any[]; after?: any[] }] | boolean;
-  createMany: [string, { before?: any[]; after?: any[] }] | boolean;
-  updateOne: [string, { before?: any[]; after?: any[] }] | boolean;
-  replaceOne: [string, { before?: any[]; after?: any[] }] | boolean;
-  getOne: [string, { before?: any[]; after?: any[] }] | boolean;
-  getMany: [string, { before?: any[]; after?: any[] }] | boolean;
-  deleteOne: [string, { before?: any[]; after?: any[] }] | boolean;
+  createOne: [string, { before?: any[]; after?: any[] }?] | boolean;
+  createMany: [string, { before?: any[]; after?: any[] }?] | boolean;
+  updateOne: [string, { before?: any[]; after?: any[] }?] | boolean;
+  replaceOne: [string, { before?: any[]; after?: any[] }?] | boolean;
+  getOne: [string, { before?: any[]; after?: any[] }?] | boolean;
+  getMany: [string, { before?: any[]; after?: any[] }?] | boolean;
+  deleteOne: [string, { before?: any[]; after?: any[] }?] | boolean;
 }
