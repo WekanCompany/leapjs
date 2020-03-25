680ff511298d719042b09e98a336fd58f429cead
Sanchan Moses <sanchanm@wekancode.com>
2020-03-25 17:12:04 +0000

    refactor(crud): removed middleware from ICrudControllerOptions

--- packages/crud/src/interfaces/crud-controller.ts
+++ packages/crud/src/interfaces/crud-controller.ts
@@ -2,7 +2,6 @@ import { ICrudControllerMethods } from './crud-controller-methods';
 
 export interface ICrudControllerOptions {
   baseRoute: string;
-  middleware?: { before: any[]; after: any[] };
   methods?: ICrudControllerMethods;
   softDelete?: boolean;
 }
