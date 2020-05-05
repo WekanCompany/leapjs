2472ca4e070083bf7b4b3508309c5b9f855374ec
Sanchan Moses <sanchanm@wekancode.com>
2020-05-05 09:05:23 +0000

    fix(router): both target & method passed into get/set metadata

--- packages/router/src/decorators/after.ts
+++ packages/router/src/decorators/after.ts
@@ -4,7 +4,11 @@ function UseAfter(middleware: Function): Function {
   return function wrapper(target: Function, method: any): void {
     const existingMetadata: any = [];
     const klass = method ? target[method] : target;
-    const middlewares = Reflect.getMetadata(LEAP_ROUTER_MIDDLEWARE, klass);
+    const middlewares = Reflect.getMetadata(
+      LEAP_ROUTER_MIDDLEWARE,
+      target,
+      method,
+    );
     if (middlewares) {
       existingMetadata.push(...middlewares);
     }
@@ -13,7 +17,12 @@ function UseAfter(middleware: Function): Function {
       type: 'after',
       target: klass,
     });
-    Reflect.defineMetadata(LEAP_ROUTER_MIDDLEWARE, existingMetadata, klass);
+    Reflect.defineMetadata(
+      LEAP_ROUTER_MIDDLEWARE,
+      existingMetadata,
+      target,
+      method,
+    );
   };
 }
 
--- packages/router/src/decorators/before.ts
+++ packages/router/src/decorators/before.ts
@@ -4,7 +4,11 @@ function UseBefore(middleware: Function): Function {
   return function wrapper(target: Function, method: any): void {
     const existingMetadata: any = [];
     const klass = method ? target[method] : target;
-    const middlewares = Reflect.getMetadata(LEAP_ROUTER_MIDDLEWARE, klass);
+    const middlewares = Reflect.getMetadata(
+      LEAP_ROUTER_MIDDLEWARE,
+      target,
+      method,
+    );
     if (middlewares) {
       existingMetadata.push(...middlewares);
     }
@@ -13,7 +17,12 @@ function UseBefore(middleware: Function): Function {
       type: 'before',
       target: klass,
     });
-    Reflect.defineMetadata(LEAP_ROUTER_MIDDLEWARE, existingMetadata, klass);
+    Reflect.defineMetadata(
+      LEAP_ROUTER_MIDDLEWARE,
+      existingMetadata,
+      target,
+      method,
+    );
   };
 }
 
